9e09f6b462c6bbf0bc582ec47da6b8f987098409
Eduart2021 <zeedymarina@yahoo.com>
2025-11-21 21:17:07 -0600

    reupload util

---
+++ src/util/errorHandler.ts
@@ -0,0 +1,17 @@
+
+
+export class NetworkError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'NetworkError from error class';
+  }
+}
+
+export class DataError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'DataError from error class';
+  }
+}
+
+
